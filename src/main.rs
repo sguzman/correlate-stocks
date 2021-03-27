0968cb0266694bf629fe66b3aeaee2c5d99338ca
Salvador Guzman <guzmansalv@gmail.com>
2021-03-27 10:06:20 -0700

    use hash to pair up with dates

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-extern crate chrono;
 extern crate csv;
 extern crate serde;
 
@@ -6,6 +5,9 @@ use serde::{Serialize, Deserialize};
 use std::env;
 use csv::Reader;
 use chrono::{Date, DateTime, TimeZone, NaiveDate};
+use std::collections::HashMap;
+
+type Hashy = HashMap<String, f64>;
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Day {
@@ -16,12 +18,6 @@ struct Day {
     low: f32,
     close: f64,
     adjclose: f32
-}
-
-#[derive(Debug)]
-struct Data {
-    date: NaiveDate,
-    close: f64
 }
 
 fn dot(a: &Vec<f64>, b: &Vec<f64>) -> f64 {
@@ -35,45 +31,36 @@ fn dot(a: &Vec<f64>, b: &Vec<f64>) -> f64 {
     return result;
 }
 
-fn get_recs(path: &str) -> Vec<Data> {
-    let csv = std::fs::read_to_string(path).expect("Bad file");
+fn get_hash(path: &str) -> Hashy {
+    let csv: String = std::fs::read_to_string(path).expect("Bad file");
     let mut reader = csv::Reader::from_reader(csv.as_bytes());
 
-    let mut data = Vec::new();
+    let mut data: Hashy = HashMap::new();
     for record in reader.deserialize() {
         let record: Day = record.unwrap();
-
-        let date = NaiveDate::parse_from_str(&*record.date, "%Y-%m-%d").unwrap();
-        let close = record.close;
-
-        let value = Data {
-            date,
-            close
-        };
-        data.push(value);
+        data.insert(record.date, record.close);
     }
 
-    return data
+    return data;
 }
 
 fn arg(args: &Vec<String>, index: u32) -> &str {
     let file: &str =  args.get(index).expect("No file found");
-
-    return file
+    return file;
 }
 
-fn build(index: u32) -> Vec<Data> {
+fn build(index: u32) -> Hashy {
     let args: &Vec<String> = env::args().collect();
-    let a: &str = arg(args, 1);
-    let data: Vec<Data> = get_recs(a);
+    let path: &str = arg(args, index);
+    let data: Hashy = get_hash(path);
 
     return data;
 }
 
 fn main() -> Result<(), csv::Error> {
     let args: &Vec<String> = env::args().collect();
-    let a: Vec<Data> = build(1);
-    let b: Vec<Data> = build(2);
+    let a: Hashy = build(1);
+    let b: Hashy = build(2);
 
     /*
     let result = dot(&a, &b) / (dot(&a, &a) * dot(&b, &b));
